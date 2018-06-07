8490898b05a42a09d1bfd1a6b9bc40ea73d73375
Alysa Barbarisi <baal02@cox.net>
2018-06-07 22:18:18 +0000

    automatically backed up by learn

---
+++ oo_basics.rb
@@ -0,0 +1,53 @@
+class Book
+  
+  attr_accessor :title, :author, :page_count, :genre, :main_character
+  
+  def initialize(title, author, page_count, genre)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+    
+ end
+
+# def title
+#   @title 
+# end
+
+# def title=(title)
+#   @title=title
+# end
+
+# def author
+#   @author 
+# end
+
+# def author=(author)
+#   @title=title 
+# end 
+
+# def page_count
+#   @page_count
+# end
+
+# def page_count=(page_count)
+#   @page_count=page_count
+# end 
+
+# def genre
+#   @genre
+# end
+
+# def genre=(genre)
+#   @genre=genre
+# end
+
+end 
+
+book1 = Book.new("The Giver", "Lois Lowry", 300, "Fiction")
+book2 = Book.new("Catcher in the Rye", "JD Salinger", 102, "Literay realism")
+book3 = Book.new("Mockingjay", "Suzanne Collins", 340, "Science Fiction")
+
+book2.author = "JD Chan"
+book2.main_character = "Holden Coulfield"
+puts book2.main_character
